b051e1594a2029c7777b1e99a0a394ef1c712bc6
ketankokane94 <ketankokane94@gmail.com>
2019-02-05 11:37:31 -0500

    first commit
    added the file required to generate dataset

---
+++ project_1/generate_dataset.py
@@ -0,0 +1,15 @@
+import random
+import numpy as np
+
+
+def generate_data_set_a(n):
+    list_of_integers_upto_n = list(range(n))
+    random.shuffle(list_of_integers_upto_n)
+    return list_of_integers_upto_n
+
+
+def generate_data_set_b(n):
+    result = np.random.poisson(n/2,n)
+    return list(result)
+
+
