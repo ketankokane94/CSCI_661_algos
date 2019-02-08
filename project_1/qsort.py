1712ae957ce24cc8ccbe3fe411a0386b41699df9
ketankokane94 <ketankokane94@gmail.com>
2019-02-08 13:15:32 -0500

    corrected the quick sort
    going from left marker to rigfht most index
    and putting the pivot at the correct position

--- project_1/qsort.py
+++ project_1/qsort.py
@@ -1,37 +1,54 @@
 import generate_dataset
 
+
 def get_pivot_value(array,l,r):
-    return array[l]
+    return l
+
 
 def partition(array, left, right):
-    pivot_value = get_pivot_value(array,left,right)
+    #pivot = get_pivot_value(array,left,right)
+    pivot_value = array[left]
     left_marker = left
     right_marker = right
     while left_marker < right_marker:
-        while array[left_marker] < pivot_value:
+        while left_marker <= right and array[left_marker] <= pivot_value:
             left_marker += 1
-        while array[right_marker] > pivot_value:
+        while left <= right_marker and array[right_marker] > pivot_value:
             right_marker -= 1
 
         if left_marker < right_marker:
             array[left_marker], array[right_marker] = array[right_marker],array[left_marker]
-            left_marker += 1
-            right_marker - + 1
 
-    return left_marker
-
+    array[left],array[right_marker] = array[right_marker],array[left]
+    return right_marker
 
 
 def quick_sort(left,right, array):
     if left < right:
         split_point = partition(array, left, right)
-        quick_sort(left,split_point-1,array)
-        quick_sort(split_point+1,right,array)
-    print(array)
+        array = quick_sort(left,split_point-1,array)
+        array = quick_sort(split_point+1,right,array)
+    return array
 
+
+def verify(array):
+
+    for i in range(len(array) -1):
+        if array[i] <= array[i+1]:
+            i+=1
+        else:
+            print('Not sorted')
+            print(array[i],array[i+1])
+
+            return
 
 if __name__ == "__main__":
-    for N in [3,5,10]:
-        quick_sort(0, N-1,generate_dataset.generate_data_set_b(N))
+    for N in [1000,10000,50000,100000,500000]:
+    #for N in [21]:
+        array = quick_sort(0, N-1,generate_dataset.generate_data_set_b(N))
+        print("Verifying array of size " ,N )
+        verify(array)
+
+
 
 
