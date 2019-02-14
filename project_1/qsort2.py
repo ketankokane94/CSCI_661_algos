70f1be86d0659a0239f7beab922b7832b0a27967
ketankokane94 <ketankokane94@gmail.com>
2019-02-13 20:26:52 -0500

    completed algo implementation

--- project_1/qsort2.py
+++ project_1/qsort2.py
@@ -1,64 +1,29 @@
-import generate_dataset
-
-def find_min(unsorted_list,from_index=1):
-    min = 99999999
-    min_element_index = 0
-    # iterate from given index upto the length of list
-    for index in range(from_index,len(unsorted_list)):
-        # every if condition can be considered as an comparison made
-
-        if unsorted_list[index] < min:
-            # if a smaller element compared to min is found then update it
-            min = unsorted_list[index]
-            min_element_index = index
-    # return the minimum element, its zero based index from the list and
-    # number of comparisons required to find the min element
-    return min,min_element_index
+import generate_dataset, qsort
 
 
+def insertion_sort(arr):
 
-def ssort(unsorted_list):
-    """
-    this function sorts an unsorted list using selection sort
-    :param unsorted_list:
-    :return:  a tuple of sorted list and number of comparisons, eg ([1,2],3)
-    """
+    for i in range(len(arr)):
+        cursor = arr[i]
+        pos = i
 
-    for position_to_sort in range(len(unsorted_list)-1):
-        min,index  = find_min(unsorted_list,position_to_sort)
-        if unsorted_list[position_to_sort]  > min:
-            unsorted_list[position_to_sort], unsorted_list[index] = unsorted_list[index], unsorted_list[position_to_sort]
-    return  unsorted_list
+        while pos > 0 and arr[pos - 1] > cursor:
+            # Swap the number down the list
+            arr[pos] = arr[pos - 1]
+            pos = pos - 1
+        # Break and do the final swap
+        arr[pos] = cursor
 
-
-def get_pivot_value(array,l,r):
-    return l
-
-
-def partition(array, left, right):
-    pivot_value = array[left]
-    left_marker = left
-    right_marker = right
-    while left_marker < right_marker:
-        while left_marker <= right and array[left_marker] <= pivot_value:
-            left_marker += 1
-        while left <= right_marker and array[right_marker] > pivot_value:
-            right_marker -= 1
-
-        if left_marker < right_marker:
-            array[left_marker], array[right_marker] = array[right_marker],array[left_marker]
-
-    array[left],array[right_marker] = array[right_marker],array[left]
-    return right_marker
+    return arr
 
 
 def quick_sort(left,right, array):
-    if left < right and len(array) > 9:
-        split_point = partition(array, left, right)
+    if left < right and (right - left + 1) > 10:
+        split_point = qsort.partition(array, left, right)
         array = quick_sort(left,split_point-1,array)
         array = quick_sort(split_point+1,right,array)
     elif left < right:
-        array = ssort(array)
+        array[left:right+1] = insertion_sort(array[left:right+1])
     return array
 
 
@@ -76,8 +41,14 @@ def verify(array):
 
 if __name__ == '__main__':
     pass
-    #, 10000, 50000, 100000, 500000
-    for N in [100]:
-        array = quick_sort(0, N - 1, generate_dataset.generate_data_set_a(N))
-        verify(array)
-    print(array)
+
+
+'''
+quick sort algorithm for dataset B
+Input Length         CPU Time            	
+1000                 0.0033557916942395663	
+10000                0.0572687073757774  	
+50000                0.468938852611341   	
+100000               1.3424009774860584  	
+500000               11.633198775743184 
+'''
