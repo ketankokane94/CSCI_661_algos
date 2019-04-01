26f8948de0164ee9a5afaa5a874d8bb0fd1e4fb7
ketankokane94 <ketankokane94@gmail.com>
2019-04-01 13:19:01 -0400

    added constants to sve merged file and frequency estimate object

--- project_2/code/src/constants.java
+++ project_2/code/src/constants.java
@@ -2,4 +2,6 @@ public  class constants {
 
     public  final static String SERIALIZED_FILE_NAME = "rootNodeSerialized.txt";
     public final  static String ENCODED_FILE_NAME = "E.txt";
+    public  final  static  String FREQUENCY_ESTIMATION_FILE = "/data/merged.txt";
+    public  final  static  String FREQUENCY_HASH_MAP_FILE = "/data/estimatedFrequency.txt";
 }
