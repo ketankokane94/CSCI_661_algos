af7d755ce5a3c1a23ab67c02eec395f4ee09171a
ketankokane94 <ketankokane94@gmail.com>
2019-04-02 06:16:09 -0400

    code cleaning

--- project_2/code/src/Driver.java
+++ project_2/code/src/Driver.java
@@ -3,16 +3,24 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Driver {
+    private static final int RUNS = 11;
+    private List<Long> initialFileSize;
+    private List<Long> encodeTime;
+    private List<Long> decodeTime;
+    private List<Long> afterCompressionFileSize;
 
-    private void RunExperimentForFile(String file,boolean useEstimatedFrequency) throws IOException {
+    public Driver() {
+        initialFileSize = new ArrayList<>();
+        encodeTime = new ArrayList<>();
+        decodeTime = new ArrayList<>();
+        afterCompressionFileSize = new ArrayList<>();
+    }
 
-        List<Long> initialFileSize = new ArrayList<>();
-        List<Long> encodeTime = new ArrayList<>();
-        List<Long> decodeTime = new ArrayList<>();
-        List<Long> afterCompressionFileSize = new ArrayList<>();
+    private void RunExperimentForFile(String file, boolean useEstimatedFrequency) throws IOException {
+
         Encode encode = new Encode(file);
         Decode decode = new Decode("decoded_.txt");
-        for (int runCount = 0; runCount < 1; runCount++) {
+        for (int runCount = 0; runCount < Driver.RUNS; runCount++) {
             initialFileSize.add(encode.getFileSize(file));
             long startTime = System.nanoTime();
             encode.encodeFile(useEstimatedFrequency);
@@ -25,39 +33,58 @@ public class Driver {
             endTime = System.nanoTime();
             duration = (endTime - startTime);
             decodeTime.add(duration);
-            Helper.deleteFiles();
+            //Helper.deleteFiles();
         }
-          //divide by 1000000 to get milliseconds.
-        double temp = 0.0;
+        //divide by 1000000 to get milliseconds.
+        double temp = getAverage(initialFileSize);
+        double compressionRatio = 0.0;
         for (int i = 0; i < initialFileSize.size(); i++) {
-            temp += (((double) initialFileSize.get(i) / afterCompressionFileSize.get(i)));
+            compressionRatio += (((double) initialFileSize.get(i) / afterCompressionFileSize.get(i)));
         }
-        System.out.println(temp / 1);
-        temp = 0.0;
-        for (int i = 0; i < encodeTime.size(); i++) {
-            temp += (((double) encodeTime.get(i)));
+        compressionRatio = compressionRatio / Driver.RUNS;
+
+        double ecodingTime = getAverage(encodeTime) / 1000000;
+        double decodingTime = getAverage(decodeTime) / 1000000;
+        System.out.format("%20s %20f %20s %20s", file, compressionRatio, ecodingTime, decodingTime);
+        System.out.println();
+    }
+
+    private double getAverage(List<Long> arrayList) {
+
+        double sum = 0.0;
+        for (int i = 0; i < arrayList.size(); i++) {
+            sum += (((double) arrayList.get(i)));
         }
-
-        System.out.println("Encoding time " + (temp/1000000));
-
-        temp = 0.0;
-        for (int i = 0; i < decodeTime.size(); i++) {
-            temp += (((double) decodeTime.get(i)));
-        }
-
-        System.out.println("decodeTime time " + (temp/1000000));
+        sum = sum / Driver.RUNS;
+        return sum;
     }
 
 
     public static void main(String[] args) {
-        try {
-            Driver driver = new Driver();
-            driver.RunExperimentForFile("data/001.txt", true);
-        } catch (FileNotFoundException ex) {
-            System.err.println("file not found");
-            ex.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
+        System.out.format("%20s %20s%20s%20s", "fileName", "compressionRatio", "encodingTime", "Decoding Time");
+        System.out.println();
+        String[] files = getFilesInsideDataDirectory();
+        if (files != null) {
+            for (int i = 0; i < files.length; i++) {
+                try {
+                    Driver driver = new Driver();
+                    driver.RunExperimentForFile("data/"+ files[i], false);
+                } catch (FileNotFoundException ex) {
+                    System.err.println("file not found");
+                    ex.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
         }
+    }
+
+    private static String[] getFilesInsideDataDirectory() {
+        File file = new File("./data");
+        if (file.isDirectory()) {
+            final String[] list = file.list();
+            return list;
+        }
+        return null;
     }
 }
