5409f0c70b47b8e2754400215bd60d30a361aca6
ketankokane94 <ketankokane94@gmail.com>
2019-06-25 16:34:00 +0530

    solution for palindrome permutation problem

--- data_structures/stringqts.java
+++ data_structures/stringqts.java
@@ -4,16 +4,20 @@
  Problems:
  URLify a string(replace ' ' with '%20')
  RemoveChars (remove a list of characters from a given string)
+ Reverse Words in a string
+ Palindrome permuation
   */
 import java.util.HashSet;
 
 public class stringqts {
     public static void main(String[] args) {
 
+        System.out.println(('0' - '3'));
         String result = URLify("abc  b a        ", 8);
         System.out.println(result);
         System.out.println(removeChars("aaaaade","ad"));
         reverseWords("ketan am i");
+        System.out.println(PalindromePermutation("tactcao"));
     }
 
     /**
@@ -108,5 +112,39 @@ public class stringqts {
             input = reverse(input, left, right-1);
         }
         return new String(input);
+    }
+
+    /**
+     * Palindrome Permutation: Given a string, 
+     * write a function to check if it is a permutation of a palin­ drome.
+     *  A palindrome is a word or phrase that is the same forwards and backwards. A permutation is a rearrangement of letters. 
+     * The palindrome does not need to be limited to just dictionary words.
+     * @param input
+     * @return
+     */
+    public static boolean PalindromePermutation(String input){
+        input = input.toLowerCase();
+        int [] occurances = new int [256];
+        int index = 0;
+        boolean oddLengthString =false;
+        for(; index < input.length(); index ++){ 
+            occurances[(int)input.charAt(index)] = occurances[(int)input.charAt(index)] + 1; 
+        }
+
+        if (index % 2 != 0 ){
+            oddLengthString = true;
+        }
+
+        for (int i = 0; i < 256; i++) {
+            if(occurances[i]%2 != 0){
+                if (oddLengthString){
+                    oddLengthString = false;
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 }
